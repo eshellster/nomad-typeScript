95d76e4ffaf0f490aa8edd97a1c3325a0a139f5b
eshell <eshellster@gmail.com>
2019-10-21 16:43:51 +0900

    블록체인 평션 1

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const jaurim = new Human("jaurim", 24, "female");
+const genesisBlock: Block = new Block(
+  0,
+  "20202020020202020",
+  "",
+  "Hello",
+  123456
+);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+let blockchain: Block[] = [genesisBlock];
 
-console.log(sayHi(jaurim));
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
