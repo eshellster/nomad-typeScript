180d0f0de94695f7cf86ec6873c76b857b38a70f
eshell <eshellster@gmail.com>
2019-10-22 19:01:08 +0900

    make blockchain

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,13 @@ class Block {
   ): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+  static validateStucture = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
   constructor(
     index: number,
     hash: string,
@@ -45,5 +52,60 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const creatNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calcuateBlockHash(
+    newIndex,
+    previosBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previosBlock.hash,
+    data,
+    newTimeStamp
+  );
+  addBlock(newBlock);
+  return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calcuateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previosBlock: Block): boolean => {
+  if (!Block.validateStucture(candidateBlock)) {
+    return false;
+  } else if (previosBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previosBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+creatNewBlock("두-번째");
+creatNewBlock("세-번째");
+creatNewBlock("네-번째");
+
+console.log(blockchain);
 
 export {};
